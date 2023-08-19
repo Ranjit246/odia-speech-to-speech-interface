16f7a76d607fdb493c637cf2978e5c3de591af6d
Ranjit Patro <ranjitp20@iiserbpr.ac.in>
2023-08-19 19:32:31 +0530

    Add files via upload

---
+++ app_bot.py
@@ -0,0 +1,80 @@
+### Import Dependencies
+import tempfile
+from ttsmms import TTS
+import soundfile as sf
+import io
+import numpy as np
+from fastapi.responses import FileResponse
+import librosa
+import shutil
+import torch
+from transformers import Wav2Vec2ForCTC, AutoProcessor, AutoTokenizer, AutoModelForCausalLM
+
+################### STT Part ######################
+
+model_id = "facebook/mms-1b-all"
+processor = AutoProcessor.from_pretrained(model_id)
+model = Wav2Vec2ForCTC.from_pretrained(model_id)
+# Add language adapters for Odia ('ory')
+processor.tokenizer.set_target_lang("ory")
+model.load_adapter("ory")
+
+audio = "/home/server_admin/Ranjit-RP/or_bot/Odia_i_5.wav"
+
+def transcribe(audio):
+    y, _ = librosa.load(audio)
+    inputs = processor(y, sampling_rate=16000, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs).logits
+    ids = torch.argmax(outputs, dim=-1)[0]
+    text = processor.decode(ids)
+    return  text
+
+############# LLM Part ###############
+
+# Specify input
+# text = "ଓଡ଼ିଶାରେ କେଉଁ ପ୍ରକାରର ଜଙ୍ଗଲ ରହିଛି?"
+
+text1 = transcribe(audio)
+
+# Specify device 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Load model and tokenizer 
+tokenizer = AutoTokenizer.from_pretrained("Ranjit/llama_v2_or")
+model = AutoModelForCausalLM.from_pretrained("Ranjit/llama_v2_or", trust_remote_code=True, torch_dtype=torch.float16).to(device)
+
+# Tokenize input 
+inputs = tokenizer(text1, return_tensors="pt").to(device)
+
+# Generate output
+with torch.no_grad():
+  outputs = model.generate(input_ids=inputs["input_ids"], 
+                           attention_mask=inputs["attention_mask"],
+                           max_new_tokens=1024, 
+                           pad_token_id=tokenizer.eos_token_id)
+
+# Decode and print  
+# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+text2 = tokenizer.decode(outputs[0], skip_special_tokens=True)
+################## TTS Part #####################
+
+tts = TTS("ory")
+
+# def generate_audio(text):
+#     wav = tts.synthesis(text)
+#     temp_name = next(tempfile._get_candidate_names())
+#     audio_path = temp_name+".wav"
+#     sf.write(audio_path, wav["x"], wav["sampling_rate"])
+#     return FileResponse(path=audio_path, filename=audio_path, media_type='text/wav')
+
+def generate_audio(text):
+    wav = tts.synthesis(text)
+    audio_file = tempfile.NamedTemporaryFile(suffix=".wav", delete=False)
+    audio_path = audio_file.name
+    sf.write(audio_path, wav["x"], wav["sampling_rate"])
+    return audio_path
+
+a = generate_audio(text2)
+destination_file = "/home/server_admin/Ranjit-RP/or_bot/out.wav"
+shutil.copy(a, destination_file)
